7ec8a9a7785c534b9f15444b55b6f89a80cd6a30
a.tsanev <syzenew@gmail.com>
2025-02-06 22:48:52 +0200

    adjust setup/local

--- go.local.sh
+++ go.local.sh
@@ -1,9 +1,21 @@
 #!/usr/bin/env bash
 
-export PATH=$PATH
-PATH:"$(pwd)"
-rm -rf ./venv
-pip3 install --upgrade pip
-python3 -m venv venv
-source ./venv/bin/activate
+set -e  
+
+export PATH=$PATH:"$(pwd)"
+
+if [ -d "./venv" ]; then
+    echo "Removing existing virtual environment..."
+    rm -rf ./venv
+fi
+
+echo "Upgrading pip and setting up virtual environment..."
+pip install --upgrade pip --user
+python -m venv venv
+
+source ./venv/Scripts/activate
+
+echo "Installing the package..."
 pip3 install .
+
+echo "Setup complete. Virtual environment is ready."
--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='mypackage',
+    name='pytest_demo',
     version='0.0.1',
+    description='Pytest demo tests',
+    author='Sam',
+    packages=['test'],  # Use the actual directory name containing your package
     install_requires=[
         'requests', 'responses',
         'jsonpath-rw', 'pytest', 'jsonpath-ng', 'jsonpath_rw_ext', 'requests-toolbelt', 'pytest-html', 'pytest-messenger', 'python-dateutil', 'Faker',
-    ],
+    ]
 )
